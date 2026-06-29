6003c4ca4b73f92428020c1f8c441b4974735e32
akalya-tech155 <akalyavlab182@gmail.com>
2026-06-29 16:59:26 +0530

    Payment Module Completed

---
+++ pages/PaymentPage.ts
@@ -0,0 +1,48 @@
+
+/*
+
+Scenario
+
+Patient Login
+
+↓
+
+Open Appointment
+
+↓
+
+Pay
+
+↓
+
+Card Number
+
+Expiry
+
+CVV
+
+↓
+
+Pay Now
+
+↓
+
+Payment Success
+
+Methods
+
+enterCard()
+
+enterExpiry()
+
+enterCVV()
+
+clickPay()
+
+paymentSuccess()
+
+Assertion
+
+Payment Successful
+
+*/
