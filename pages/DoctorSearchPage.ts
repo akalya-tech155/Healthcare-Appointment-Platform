9d5a1e114c183936603224730bdc82c8002eb512
akalya-tech155 <akalyavlab182@gmail.com>
2026-06-29 16:53:33 +0530

    Doctor Search Completed

---
+++ pages/DoctorSearchPage.ts
@@ -0,0 +1,37 @@
+/*
+Scenario
+
+Search by
+
+Cardiologist
+
+Location
+
+Experience
+
+Click Search
+
+Doctor List appears
+
+
+Methods
+
+searchSpecialization()
+
+searchLocation()
+
+clickSearch()
+
+doctorList()
+
+
+Assertions
+
+Doctor list visible
+
+Doctor name visible
+
+Hospital visible
+
+Experience visible
+*/
