0735dadd8574c71cf7b27aa06fd8f9cd155f5628
akalya-tech155 <akalyavlab182@gmail.com>
2026-06-29 16:55:48 +0530

    Appointment Booking Completed

---
+++ pages/AppointmentPage.ts
@@ -0,0 +1,47 @@
+/*
+
+Scenario
+
+Login
+
+↓
+
+Search Doctor
+
+↓
+
+Select Doctor
+
+↓
+
+Choose Date
+
+↓
+
+Choose Time
+
+↓
+
+Book Appointment
+
+↓
+
+Confirmation
+
+Methods
+
+selectDoctor()
+
+selectDate()
+
+selectTime()
+
+bookAppointment()
+
+confirmationMessage()
+
+Validation
+
+Appointment Booked Successfully
+
+*/
