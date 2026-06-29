6801ae2312c999e4f61d9e065daf213412ad3876
akalya-tech155 <akalyavlab182@gmail.com>
2026-06-29 16:49:29 +0530

    Patient Registration Completed

---
+++ pages/PatientRegistrationPage.ts
@@ -0,0 +1,42 @@
+/*
+Scenario
+
+Patient opens website.
+
+Clicks Register.
+
+Enters
+
+First Name
+
+Last Name
+
+Email
+
+Phone
+
+Password
+
+Clicks Register.
+
+Registration Successful.
+*/
+
+// examples
+/*
+goto()
+
+enterFirstName()
+
+enterLastName()
+
+enterEmail()
+
+enterPhone()
+
+enterPassword()
+
+clickRegister()
+
+successMessage()
+*/
---
+++ tests/patient.spec.ts
@@ -0,0 +1,22 @@
+/*
+test('Patient Registration', async ({ page }) => {
+
+await patient.goto();
+
+await patient.enterFirstName("Akalya");
+
+await patient.enterLastName("S");
+
+await patient.enterEmail("akalya@test.com");
+
+await patient.enterPhone("9876543210");
+
+await patient.enterPassword("Test@123");
+
+await patient.clickRegister();
+
+await expect(page.locator(".success"))
+.toHaveText("Registration Successful");
+
+});
+*/
