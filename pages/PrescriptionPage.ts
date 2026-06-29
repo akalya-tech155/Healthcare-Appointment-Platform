e1148815da971a44e69f1177c462265ac0906a74
akalya-tech155 <akalyavlab182@gmail.com>
2026-06-29 16:58:00 +0530

    Prescription Module Completed

---
+++ pages/PrescriptionPage.ts
@@ -0,0 +1,37 @@
+/*
+
+Scenario
+
+Doctor Login
+
+↓
+
+Open Today's Appointments
+
+↓
+
+Select Patient
+
+↓
+
+Write Prescription
+
+↓
+
+Save
+
+Methods
+
+openAppointment()
+
+enterMedicine()
+
+enterDosage()
+
+savePrescription()
+
+Assertion
+
+Prescription Saved Successfully
+
+*/
