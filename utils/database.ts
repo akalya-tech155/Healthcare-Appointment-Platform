67889b19ddb7a7279e6e68760b1eaafbcbde6fb9
akalya-tech155 <akalyavlab182@gmail.com>
2026-06-30 17:52:10 +0530

    Update database.ts

---
+++ utils/database.ts
@@ -0,0 +1,11 @@
+/*
+
+import { Database } from '../utils/Database';
+
+const result = await Database.executeQuery(
+  "SELECT * FROM Appointments WHERE PatientName = 'Akalya'"
+);
+
+expect(result.length).toBeGreaterThan(0);
+
+*/
